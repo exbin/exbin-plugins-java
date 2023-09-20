5eeeba5ee787bc80e096f624d93ebd8c44b8e144
hajdam <hajdam@users.sf.net>
2023-09-20 23:33:33 +0200

    Added VAqua and Napkin lafs

---
+++ plugins/exbin-framework-napkin-laf/src/main/java/org/exbin/framework/plugins/napkin_laf/NapkinLafModule.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) ExBin Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.exbin.framework.plugins.napkin_laf;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.ParametersAreNonnullByDefault;
+import javax.swing.UIManager;
+import org.exbin.xbup.plugin.LookAndFeelApplier;
+import org.exbin.framework.api.XBApplication;
+import org.exbin.framework.api.XBApplicationModule;
+import org.exbin.xbup.plugin.XBModuleHandler;
+
+/**
+ * Napkin look and feel plugin.
+ *
+ * @author ExBin Project (https://exbin.org)
+ */
+@ParametersAreNonnullByDefault
+public class NapkinLafModule implements XBApplicationModule, LookAndFeelApplier {
+
+    public static final String NAPKIN_LAF_CLASS = "net.sourceforge.napkinlaf.NapkinLookAndFeel";
+    private XBApplication application;
+
+    public NapkinLafModule() {
+    }
+
+    @Override
+    public void init(XBModuleHandler moduleHandler) {
+        this.application = (XBApplication) moduleHandler;
+
+        UIManager.installLookAndFeel("Napkin", NAPKIN_LAF_CLASS);
+        application.registerLafPlugin(NAPKIN_LAF_CLASS, this);
+    }
+
+    @Override
+    public void applyLookAndFeel(String className) {
+        try {
+            UIManager.setLookAndFeel(NAPKIN_LAF_CLASS);
+        } catch (Exception ex) {
+            Logger.getLogger(NapkinLafModule.class.getName()).log(Level.SEVERE, "Failed to initialize LaF", ex);
+        }
+    }
+
+    @Override
+    public void unregisterModule(String moduleId) {
+    }
+}
---
+++ plugins/exbin-framework-vaqua-laf/src/main/java/org/exbin/framework/plugins/vaqua_laf/VAquaLafModule.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) ExBin Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.exbin.framework.plugins.vaqua_laf;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import javax.swing.UIManager;
+import org.exbin.xbup.plugin.LookAndFeelApplier;
+import org.exbin.framework.api.XBApplication;
+import org.exbin.framework.api.XBApplicationModule;
+import org.exbin.xbup.plugin.XBModuleHandler;
+
+/**
+ * VAqua look and feel plugin.
+ *
+ * @author ExBin Project (https://exbin.org)
+ */
+@ParametersAreNonnullByDefault
+public class VAquaLafModule implements XBApplicationModule, LookAndFeelApplier {
+
+    public static final String VAQUA_LAF_CLASS = "org.violetlib.aqua.AquaLookAndFeel";
+    private XBApplication application;
+
+    public VAquaLafModule() {
+    }
+
+    @Override
+    public void init(XBModuleHandler moduleHandler) {
+        this.application = (XBApplication) moduleHandler;
+
+        if (System.getProperty("os.name", "").startsWith("Mac OS")) {
+            UIManager.installLookAndFeel("VAqua", VAQUA_LAF_CLASS);
+            application.registerLafPlugin(VAQUA_LAF_CLASS, this);
+        }
+    }
+
+    @Override
+    public void applyLookAndFeel(String className) {
+        try {
+            UIManager.setLookAndFeel(VAQUA_LAF_CLASS);
+        } catch (Exception ex) {
+            System.err.println("Failed to initialize LaF");
+        }
+    }
+
+    @Override
+    public void unregisterModule(String moduleId) {
+    }
+}
